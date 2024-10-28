ec74a8bfe5c8d50b16fa076aa097a7a86bb7b425
subash <subashchandraboseravi45@gmail.com>
2024-10-27 22:58:27 -0700

    dummy routes

---
+++ backend/prisma/migrations/20241028051747_s/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "user";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "gender" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,78 @@
+const { PrismaClient } = require('@prisma/client');
 const express  = require('express');
 const app  = express();
 const PORT = 3001 | process.env.PORT;
 
 
-app.get('/' , (req, res) => {
-    return res.json({
-        message : 'server'
-    })
+
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.post('/signup' , async  (req, res) => {
+
+    const {username , email , password , age , gender} = await req.body;
+ 
+    try{
+        await prisma.user.create({
+           data : {
+            name : username,
+            email : email,
+            password : password,
+            age : age,
+            gender : gender
+        }
+       })
+       res.json({
+        message : 'user saved sucessfully'
+       })
+    }
+     catch(E){
+        res.json({
+        message : 'something went wrong'
+      })
+    }
+   
+   
 })
 
+app.post('/user' , async (req,res) => {
+    const {email , name} = await req.body;
+
+    try { 
+        const user = await prisma.user.findUnique({
+           where : {
+            email
+           }
+        })
+                
+        if (user) { 
+
+            return res.json({
+                message: 'User found',
+                user: user
+            });
+        } else {
+            return res.status(404).json({ 
+                message: 'User not found'
+            });
+        }
+    } catch (e) {
+        console.error(e); 
+        return res.status(500).json({ 
+            message: 'An error occurred while searching for the user'
+        });
+    }
+})
+
+
+app.get('/getalluser' , async (req, res) => {
+     const users = await prisma.user.findMany({})
+
+     res.json({
+        users : users
+     })
+})
 
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`)
